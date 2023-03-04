e60b9ca92550f8382898c913a30814ede0c0032f
daniertg <febriandani00@gmail.com>
2023-03-04 11:10:36 +0700

    make

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdukController extends Controller
+{
+    //
+    
+public function index1()
+{
+    return view('product');
+    
+}
+
+}
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdukController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/about-us', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
+Route::get('/news', [App\Http\Controllers\NewsController::class, 'index'])->name('news');
+
+
+Route::prefix('produk') -> group (function () {
+    Route::get('/1', [ProdukController::class, 'index1']);
+    Route::get('/2', [ProdukController::class, 'index2']);
+    Route::get('/3', [ProdukController::class, 'index3']);
+}) -> name('produk');
+
+Route::prefix('program') -> group (function () {
+    Route::get('/1', [ProdukController::class, 'index1']);
+    Route::get('/2', [ProdukController::class, 'index2']);
+    Route::get('/3', [ProdukController::class, 'index3']);
+}) -> name('program');
+
+
+Route::get('/produk', [ProdukController::class, 'index']);
